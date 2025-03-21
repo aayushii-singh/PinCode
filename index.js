1494ae84bcad6eb695f17930da78f08df9911438
aayushii-singh <122150257+aayushii-singh@users.noreply.github.com>
2025-03-21 11:17:33 +0530

    Added:no alphabet can be added

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
-function validatePin(pin) {
-    let pinRegex = /^[1-9][0-9]{5}$/; // 6-digit, no leading 0
-    if (pinRegex.test(pin)) {
-        console.log(`Entered PIN Code ${pin} is Correct`);
-    } else {
-        console.log(`Entered PIN Code ${pin} is Incorrect`);
-    }
+function validatePIN(pin) {
+    let regex = /^[0-9]{6}$/;
+    console.log(`${pin} is ${regex.test(pin) ? 'Valid' : 'Invalid'}`);
 }
 
-// Test cases
-validatePin("400088"); // Correct
-validatePin("110001"); // Correct
-validatePin("012345"); // Incorrect
-validatePin("4000A8"); // Incorrect
+validatePIN("400088"); // Valid
+validatePIN("A400088"); // Invalid
