9651035486aec18826df575a54f2685033a2dfe8
aayushii-singh <122150257+aayushii-singh@users.noreply.github.com>
2025-03-21 11:23:39 +0530

    Added: 400 088 and 400088 both are valid

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 function validatePinCode(pin) {
-    let regex = /^[0-9]{6}$/;
+    let regex = /^[0-9]{3}\s?[0-9]{3}$/;
     console.log(`${pin} is ${regex.test(pin) ? "Valid" : "Invalid"}`);
 }
 
-validatePinCode("400088");  // Valid
-validatePinCode("A400088"); // Invalid
-validatePinCode("400088B"); // Invalid
+validatePinCode("400088");   // Valid
+validatePinCode("400 088");  // Valid
+validatePinCode("A400088");  // Invalid
+validatePinCode("400088B");  // Invalid
